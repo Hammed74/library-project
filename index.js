86d706697d5faa8dfae473421ba65b79f4e6456c
Rarri74 <Hammed74@gmail.com>
2023-11-12 12:11:29 -0500

    created grid components for book list

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const list = document.querySelector(".book-list");
 const newBookButton = document.querySelector(".new-book");
 const form = document.querySelector(".form");
 
-newBookButton.addEventListener("click", () => {
-  form.style.display = "block";
-});
+newBookButton.addEventListener("click", () => {});
 
 function Book(title, author, pages, read) {
   (this.title = title),
@@ -53,16 +51,29 @@ function displayBook() {
   removeButton.setAttribute("data", `${dataAttribute}`);
   removeButton.classList.add("remove");
   statusButton.classList.add("read-toggle");
-  removeButton.textContent = "DELETE"
-  if (theLibrary[dataAttribute] == "yes"){
-  statusButton.textContent = "READ"
-  statusButton.style.backgroundColor = ""
+  removeButton.textContent = "DELETE";
+  if (theLibrary[dataAttribute].read == "yes") {
+    statusButton.textContent = "READ";
+    statusButton.style.backgroundColor = "";
   } else {
-    statusButton.textContent = "NOT YET READ"
+    statusButton.textContent = "NOT YET READ";
     statusButton.style.backgroundColor = "#ff9900";
   }
+  const authorContainer = document.createElement("div");
+  const pagesContainer = document.createElement("div");
+  const authorOfBook = document.createElement("p");
+  authorOfBook.textContent = theLibrary[theLibrary.length - 1].author;
+  const numOfPages = document.createElement("p");
+  numOfPages.textContent =
+    theLibrary[theLibrary.length - 1].pages + " " + "Pages";
+  authorContainer.classList.add("author-text-container");
+  pagesContainer.classList.add("pages-text-container");
+  authorContainer.appendChild(authorOfBook);
+  pagesContainer.appendChild(numOfPages);
   list.appendChild(bookItemContainer);
   bookItemContainer.appendChild(bookItem);
+  bookItemContainer.appendChild(authorContainer);
+  bookItemContainer.appendChild(pagesContainer);
   bookItemContainer.appendChild(removeButton);
   bookItemContainer.appendChild(statusButton);
 
@@ -76,12 +87,25 @@ function displayBook() {
   statusButton.addEventListener("click", () => {
     if (theLibrary[dataAttribute].read == "yes") {
       theLibrary[dataAttribute].read = "no";
+      statusButton.textContent = " NOT YET READ";
       statusButton.style.backgroundColor = "#ff9900";
     } else if (theLibrary[dataAttribute].read == "no") {
       theLibrary[dataAttribute].read = "yes";
-      statusButton.style.backgroundColor = ""
+      statusButton.textContent = "READ";
+      statusButton.style.backgroundColor = "";
     }
-    console.log(theLibrary)
-  })
+    console.log(theLibrary);
+  });
 }
 
+function defaultBooks(defaultTitle, defaultAuthor, defaultPages, defaultReadStatus) {
+  const myBook = new Book(defaultTitle, defaultAuthor, defaultPages, defaultReadStatus);
+  theLibrary.push(myBook);
+  console.log(theLibrary);
+  displayBook();
+}
+
+defaultBooks("Atomic Habits", "James Clear", 320, "yes");
+defaultBooks("The Great Gatsby", "F.Scott Fitzgerald", 208, "no");
+
+console.log(window.innerWidth);
